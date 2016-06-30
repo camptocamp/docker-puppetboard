1d67833aaa675405ff19d233bd9a4fe5b4a1e355
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-06-30 13:48:28 +0200

    Use v0.2.0

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-import os
-
-# Needed if a settings.py file exists
-os.environ['PUPPETBOARD_SETTINGS'] = '/app/settings.py'
-from puppetboard.app import app as application
